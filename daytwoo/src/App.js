c41dc0862b028895403a02a8c5a0c0de1ebda4a5
Marco Zagaria <marcozagaria@live.com>
2024-08-20 14:13:02 +0200

    aggiunta di bootsrtap

--- daytwoo/src/App.js
+++ daytwoo/src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="text-danger">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
