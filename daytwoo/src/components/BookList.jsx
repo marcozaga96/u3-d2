be00fde524e512ad407251c8ddc30ae4edf898c9
Marco Zagaria <marcozagaria@live.com>
2024-08-22 09:12:20 +0200

    aggiunta search

--- daytwoo/src/components/BookList.jsx
+++ daytwoo/src/components/BookList.jsx
@@ -1,20 +1,61 @@
+import { Component } from "react";
 import books from "../fantasy.json";
 import SingleBook from "./SingleBook";
-import { Col, Row, Container } from "react-bootstrap";
+import { Col, Row, Container, Form } from "react-bootstrap";
 
-const BookList = function () {
-  return (
-    <Container className="bg-secondary text-white mt-5 p-4 text-center">
-      <Row>
-        {books.map((book) => {
-          return (
-            <Col xs={12} md={6} lg={4} key={book.asin}>
-              <SingleBook book={book} />
-            </Col>
-          );
-        })}
-      </Row>
-    </Container>
-  );
-};
+// const BookList = function () {
+//   return (
+//     <Container className="bg-secondary text-white mt-5 p-4 text-center">
+//       <Row>
+//         {books.map((book) => {
+//           return (
+//             <Col xs={12} md={6} lg={4} key={book.asin}>
+//               <SingleBook book={book} />
+//             </Col>
+//           );
+//         })}
+//       </Row>
+//     </Container>
+//   );
+// };
+class BookList extends Component {
+  state = {
+    searchValue: "",
+  };
+  render() {
+    return (
+      <Container className="bg-secondary text-white mt-5 p-4 text-center">
+        <Row className="my-5">
+          <Col>
+            <Form.Control
+              type="text"
+              placeholder="cerca"
+              value={this.state.searchValue}
+              onChange={(e) => {
+                this.setState({
+                  searchValue: e.target.value,
+                });
+              }}
+            />
+          </Col>
+        </Row>
+        <Row>
+          {books
+            .filter((books) =>
+              books.title
+                .toLocaleLowerCase()
+                .includes(this.state.searchValue.toLowerCase())
+            )
+            .map((book) => {
+              return (
+                <Col xs={12} md={6} lg={4} key={book.asin}>
+                  <SingleBook book={book} />
+                </Col>
+              );
+            })}
+        </Row>
+      </Container>
+    );
+  }
+}
 export default BookList;
